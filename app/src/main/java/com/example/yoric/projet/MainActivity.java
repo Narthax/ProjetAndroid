be7c3d0d8621a69ef068909f94bec4a327360458
SimonKnu <anderlecht.11@hotmail.com>
2017-10-13 01:56:02 +0200

    TextView ok mais bug API ?

--- app/src/main/java/com/example/yoric/projet/MainActivity.java
+++ app/src/main/java/com/example/yoric/projet/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.yoric.projet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -11,12 +13,17 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.yoric.projet.adapter.CustomAdapter;
 import com.example.yoric.projet.asynctask.GetResult;
 import com.example.yoric.projet.model.ListeFilm;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements GetResult.ICallBack, View.OnClickListener {
 
@@ -24,6 +31,8 @@ public class MainActivity extends AppCompatActivity implements GetResult.ICallBa
     private Button bt_rechercher;
     private EditText et_recherche;
     private Spinner spinner;
+    private List<ListeFilm> listeFilms = new ArrayList<ListeFilm>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +47,15 @@ public class MainActivity extends AppCompatActivity implements GetResult.ICallBa
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.s_choix, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+
+
+        listeFilms.add(new ListeFilm());
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.list_layout);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+
+        CustomAdapter adapterListe = new CustomAdapter(listeFilms,MainActivity.this);
+        recyclerView.setAdapter(adapterListe);
     }
 
     @Override
@@ -60,10 +78,14 @@ public class MainActivity extends AppCompatActivity implements GetResult.ICallBa
         tv_main.setText(string);
 
         JSONObject object = new JSONObject(string);
+        Log.i("aloooo   ",object.toString());
         Gson gson = new Gson();
-        ListeFilm listeFilm = gson.fromJson(object.toString(), ListeFilm.class);
+        java.lang.reflect.Type collectionType = new TypeToken<List<ListeFilm>>() {}.getType();
+        listeFilms = gson.fromJson(object.toString(), collectionType);
 
-        Toast.makeText(this, listeFilm.getShowTitle()+ "", Toast.LENGTH_LONG).show();
+        Log.i("TEST          ffff",listeFilms.get(0).getDirector());
+
+        Toast.makeText(this, listeFilms.get(0).getShowTitle()+ "", Toast.LENGTH_LONG).show();
 
         //Gson fait
         //Suite mettre dans liste view
