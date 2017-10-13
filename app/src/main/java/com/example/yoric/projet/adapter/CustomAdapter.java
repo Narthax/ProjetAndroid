5d3788a29cbfd4cd89694a8f9c13b0964e4458b9
SimonKnu <anderlecht.11@hotmail.com>
2017-10-13 02:33:51 +0200

    dodo2

---
+++ app/src/main/java/com/example/yoric/projet/adapter/CustomAdapter.java
@@ -0,0 +1,67 @@
+package com.example.yoric.projet.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.yoric.projet.R;
+import com.example.yoric.projet.model.ListeFilm;
+
+import java.util.List;
+
+/**
+ * Created by Simon on 26-09-17.
+ */
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
+    private List<ListeFilm> list;
+    private Context context;
+
+    public CustomAdapter(List<ListeFilm> list, Context con){
+        this.list = list;
+        this.context=con;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.liste_films,parent,false);
+        return new MyViewHolder(v);
+    }
+
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        ListeFilm m = list.get(position);
+
+        holder.ivImage.setImageResource(R.drawable.a);
+        holder.tvTitre.setText(m.getShowTitle());
+        holder.tvReal.setText(m.getDirector());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        public ImageView ivImage;
+        public TextView tvTitre;
+        public TextView tvReal;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            ivImage = (ImageView) itemView.findViewById(R.id.iv_listFilm_image);
+            tvTitre = (TextView) itemView.findViewById(R.id.tv_listFilm_titre);
+            tvReal = (TextView) itemView.findViewById(R.id.tv_listFilm_realisateur);
+        }
+    }
+}
+
